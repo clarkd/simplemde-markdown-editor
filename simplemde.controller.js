28473c69fd92c354108927f3614199ae7cda8923
Dave Clarke <dave.clarke@squaredup.com>
2017-06-05 13:25:38 +0100

    Add ability to include links for Umbraco nodes

--- simplemde.controller.js
+++ simplemde.controller.js
@@ -1,7 +1,7 @@
-app.controller("squp.simplemdecontroller", function ($scope, $log, $timeout, assetsService, dialogService, mediaResource, mediaHelper) {
-	
-	assetsService.loadCss("~/app_plugins/simplemde-markdown-editor/lib/simplemde.min.css");
-	
+app.controller("squp.simplemdecontroller", function ($scope, $log, $timeout, assetsService, dialogService, contentResource) {
+
+    assetsService.loadCss("~/app_plugins/simplemde-markdown-editor/lib/simplemde.min.css");
+
     /**
 		Load & initialise SimpleMDE
 	**/
@@ -12,157 +12,183 @@ app.controller("squp.simplemdecontroller", function ($scope, $log, $timeout, ass
             element: input[0],
             initialValue: $scope.model.value,
             toolbar: [
-			{
-				name: "bold",
-				action: SimpleMDE.toggleBold,
-				className: "fa fa-bold",
-				title: "Bold",
-				default: true
-			},
-			{
-				name: "italic",
-				action: SimpleMDE.toggleItalic,
-				className: "fa fa-italic",
-				title: "Italic",
-				default: true
-			},
-			{
-				name: "strikethrough",
-				action: SimpleMDE.toggleStrikethrough,
-				className: "fa fa-strikethrough",
-				title: "Strikethrough"
-			},
-			{
-				name: "heading",
-				action: SimpleMDE.toggleHeadingSmaller,
-				className: "fa fa-header",
-				title: "Heading",
-				default: true
-			},
-			{
-				name: "heading-smaller",
-				action: SimpleMDE.toggleHeadingSmaller,
-				className: "fa fa-header fa-header-x fa-header-smaller",
-				title: "Smaller Heading"
-			},
-			{
-				name: "heading-bigger",
-				action: SimpleMDE.toggleHeadingBigger,
-				className: "fa fa-header fa-header-x fa-header-bigger",
-				title: "Bigger Heading"
-			},
-			"|",
-			{
-				name: "code",
-				action: SimpleMDE.toggleCodeBlock,
-				className: "fa fa-code",
-				title: "Code"
-			},
-			{
-				name: "quote",
-				action: SimpleMDE.toggleBlockquote,
-				className: "fa fa-quote-left",
-				title: "Quote",
-				default: true
-			},
-			{
-				name: "unordered-list",
-				action: SimpleMDE.toggleUnorderedList,
-				className: "fa fa-list-ul",
-				title: "Generic List",
-				default: true
-			},
-			{
-				name: "ordered-list",
-				action: SimpleMDE.toggleOrderedList,
-				className: "fa fa-list-ol",
-				title: "Numbered List",
-				default: true
-			},
-			{
-				name: "clean-block",
-				action: SimpleMDE.cleanBlock,
-				className: "fa fa-eraser fa-clean-block",
-				title: "Clean block"
-			},
-			"|",
-			{
-				name: "link",
-				action: SimpleMDE.drawLink,
-				className: "fa fa-link",
-				title: "Create Link",
-				default: true
-			},
-			{
-				name: "image",
-				action: $scope.insertImage, //use Umbraco's media picker for image selection
-				className: "fa fa-picture-o",
-				title: "Insert Image",
-				default: true
-			},
-			{
-				name: "table",
-				action: SimpleMDE.drawTable,
-				className: "fa fa-table",
-				title: "Insert Table"
-			},
-			{
-				name: "horizontal-rule",
-				action: SimpleMDE.drawHorizontalRule,
-				className: "fa fa-minus",
-				title: "Insert Horizontal Line"
-			},			
-			"|",
-			{
-				name: "undo",
-				action: SimpleMDE.undo,
-				className: "fa fa-undo no-disable",
-				title: "Undo"
-			},
-			{
-				name: "redo",
-				action: SimpleMDE.redo,
-				className: "fa fa-repeat no-disable",
-				title: "Redo"
-			},
-			"|",
-			{
-				name: "guide",
-				action: "https://simplemde.com/markdown-guide",
-				className: "fa fa-question-circle",
-				title: "Markdown Guide",
-				default: true
-			},
-			{
-				name: "preview",
-				action: SimpleMDE.togglePreview,
-				className: "fa fa-eye no-disable",
-				title: "Toggle Preview",
-				default: true
-			}
+                {
+                    name: "bold",
+                    action: SimpleMDE.toggleBold,
+                    className: "fa fa-bold",
+                    title: "Bold",
+                    default: true
+                },
+                {
+                    name: "italic",
+                    action: SimpleMDE.toggleItalic,
+                    className: "fa fa-italic",
+                    title: "Italic",
+                    default: true
+                },
+                {
+                    name: "strikethrough",
+                    action: SimpleMDE.toggleStrikethrough,
+                    className: "fa fa-strikethrough",
+                    title: "Strikethrough"
+                },
+                {
+                    name: "heading",
+                    action: SimpleMDE.toggleHeadingSmaller,
+                    className: "fa fa-header",
+                    title: "Heading",
+                    default: true
+                },
+                {
+                    name: "heading-smaller",
+                    action: SimpleMDE.toggleHeadingSmaller,
+                    className: "fa fa-header fa-header-x fa-header-smaller",
+                    title: "Smaller Heading"
+                },
+                {
+                    name: "heading-bigger",
+                    action: SimpleMDE.toggleHeadingBigger,
+                    className: "fa fa-header fa-header-x fa-header-bigger",
+                    title: "Bigger Heading"
+                },
+                "|",
+                {
+                    name: "code",
+                    action: SimpleMDE.toggleCodeBlock,
+                    className: "fa fa-code",
+                    title: "Code"
+                },
+                {
+                    name: "quote",
+                    action: SimpleMDE.toggleBlockquote,
+                    className: "fa fa-quote-left",
+                    title: "Quote",
+                    default: true
+                },
+                {
+                    name: "unordered-list",
+                    action: SimpleMDE.toggleUnorderedList,
+                    className: "fa fa-list-ul",
+                    title: "Generic List",
+                    default: true
+                },
+                {
+                    name: "ordered-list",
+                    action: SimpleMDE.toggleOrderedList,
+                    className: "fa fa-list-ol",
+                    title: "Numbered List",
+                    default: true
+                },
+                {
+                    name: "clean-block",
+                    action: SimpleMDE.cleanBlock,
+                    className: "fa fa-eraser fa-clean-block",
+                    title: "Clean block"
+                },
+                "|",
+                {
+                    name: "link",
+                    action: SimpleMDE.drawLink,
+                    className: "fa fa-external-link",
+                    title: "Create Link",
+                    default: true
+                },
+                {
+                    name: "internal-link",
+                    action: $scope.insertInternalLink,
+                    className: "fa fa-link",
+                    title: "Create Internal Link",
+                    default: true
+                },
+                {
+                    name: "image",
+                    action: $scope.insertImage, //use Umbraco's media picker for image selection
+                    className: "fa fa-picture-o",
+                    title: "Insert Image",
+                    default: true
+                },
+                {
+                    name: "table",
+                    action: SimpleMDE.drawTable,
+                    className: "fa fa-table",
+                    title: "Insert Table"
+                },
+                {
+                    name: "horizontal-rule",
+                    action: SimpleMDE.drawHorizontalRule,
+                    className: "fa fa-minus",
+                    title: "Insert Horizontal Line"
+                },
+                "|",
+                {
+                    name: "undo",
+                    action: SimpleMDE.undo,
+                    className: "fa fa-undo no-disable",
+                    title: "Undo"
+                },
+                {
+                    name: "redo",
+                    action: SimpleMDE.redo,
+                    className: "fa fa-repeat no-disable",
+                    title: "Redo"
+                },
+                "|",
+                {
+                    name: "guide",
+                    action: "https://simplemde.com/markdown-guide",
+                    className: "fa fa-question-circle",
+                    title: "Markdown Guide",
+                    default: true
+                },
+                {
+                    name: "preview",
+                    action: SimpleMDE.togglePreview,
+                    className: "fa fa-eye no-disable",
+                    title: "Toggle Preview",
+                    default: true
+                }
 
-		]});
-		
+            ]
+        });
+
         $('a[data-toggle="tab"]').on('shown', function (e) {
-			editor.codemirror.refresh();           
+            editor.codemirror.refresh();
         });
 
         editor.codemirror.on("change", function () {
             $scope.model.value = editor.value();
-		});
+        });
 
     });
 
     /**
-		Use Umraco's media picker to choose an image
+		Use Umbraco's media picker to choose an image
 	**/
     $scope.insertImage = function (editor) {
-        dialogService.mediaPicker({ callback: function (data) {				
-			editor.codemirror.replaceSelection('![]('+data.image+')');
-			$scope.model.value = editor.value();
-			return true;
-        }
+        dialogService.mediaPicker({
+            callback: function (data) {
+                editor.codemirror.replaceSelection('![](' + data.image + ')');
+                $scope.model.value = editor.value();
+                return true;
+            }
         });
     }
-   
+
+    /**
+        Use Umbraco's content picker to choose an node
+    **/
+    $scope.insertInternalLink = function (editor) {
+        dialogService.treePicker({
+            section: "content",
+            callback: function (item) {
+                var text = editor.codemirror.getSelection();
+                contentResource.getNiceUrl(item.id).then(function (url) {
+                    editor.codemirror.replaceSelection('[' + text + '](' + url + ')');
+                    return true;
+                });
+
+            }
+        });
+    }
+
 });
